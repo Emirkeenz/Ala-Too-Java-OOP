116cb1504d0349970f8ee432e14a00f69c6b8bc7
Emirkeenz <orozbekovjob@gmail.com>
2024-09-23 01:16:15 +0600

    added sorting method

--- Classes_and_objects_hw1/src/Book.java
+++ Classes_and_objects_hw1/src/Book.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Book {
     public String name;
     public String author;
     public float price;
     public int pages;
     public String isbn;
+    public float rating;
 
     // default constructor
     public Book(){
@@ -12,15 +16,17 @@ public class Book {
         this.price = 0.0f;
         this.pages = 0;
         this.isbn = "";
+        this.rating = 0.0f;
     }
 
     // constructor
-    public Book(String name, String author, float price, int pages, String isbn) {
+    public Book(String name, String author, float price, int pages, String isbn, float rating) {
         this.name = name;
         this.author = author;
         this.price = price;
         this.pages = pages;
         this.isbn = isbn;
+        this.rating = rating;
     }
 
     // methods
@@ -30,6 +36,24 @@ public class Book {
         System.out.println("The price of the book: " + price);
         System.out.println("The amount of pages of the book: " + pages);
         System.out.println("The isbn of the book: " + isbn);
+        System.out.println("The rating of the book: " + rating);
+    }
+
+    public static void bookRating(Book[] books) {
+        Arrays.sort(books, new Comparator<Book>() {
+            @Override
+            public int compare(Book b1, Book b2) {
+                // Compare in descending order based on rating
+                return Float.compare(b2.rating, b1.rating);
+            }
+        });
+
+        // After sorting, print the sorted books
+        System.out.println("\nBooks sorted by rating (highest to lowest):");
+        for (Book book : books) {
+            book.displayInfo();
+            System.out.println();
+        }
     }
 
     public void applyDiscount(double discount) {
--- Classes_and_objects_hw1/src/Main.java
+++ Classes_and_objects_hw1/src/Main.java
@@ -1,21 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        // Creating book objects using default and parameterized constructors
         Book book1 = new Book();  // Default constructor
-        Book book2 = new Book("Poplar in a red scarf", "Chyngyz Aitmatov", 580.99f, 544, "978-5-699-14043-5");  // Parameterized constructor
-        Book book3 = new Book("Dead souls", "Nikolai Vasilyevich Gogol", 600.00f, 464, "978-5-9925-1253-3");
+        Book book2 = new Book("Poplar in a red scarf", "Chyngyz Aitmatov", 580.99f, 544, "978-5-699-14043-5", 4.9f);
+        Book book3 = new Book("Dead souls", "Nikolai Vasilyevich Gogol", 600.00f, 464, "978-5-9925-1253-3", 4.5f);
+        Book book4 = new Book("War and Peace", "Leo Tolstoy", 750.50f, 1225, "978-3-16-148410-0", 4.8f);
+        Book book5 = new Book("Crime and Punishment", "Fyodor Dostoevsky", 500.99f, 671, "978-0-14-305814-4", 4.7f);
+        Book book6 = new Book("Moby-Dick", "Herman Melville", 450.00f, 635, "978-0-14-243724-7", 4.2f);
+        Book book7 = new Book("The Brothers Karamazov", "Fyodor Dostoevsky", 640.75f, 796, "978-0-14-044924-2", 4.9f);
 
-        // Displaying book information
-        System.out.println("Book 1:");
-        book1.displayInfo();
 
-        System.out.println("\nBook 2:");
-        book2.displayInfo();
+        Book[] books = {book1, book2, book3, book4, book5, book6, book7};
 
-        System.out.println("\nBook 3:");
-        book3.displayInfo();
+        System.out.println("Books before sorting:");
+        for (Book book : books) {
+            book.displayInfo();
+            System.out.println();
+        }
 
-        // Applying discount to a book
+        Book.bookRating(books);
+
         System.out.println("\nApplying discount to Book 2:");
         book2.applyDiscount(15);
     }
